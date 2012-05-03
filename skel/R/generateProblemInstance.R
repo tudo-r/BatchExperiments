94f4a62b052a3a4a50efa4c87bae37c9ce164a41
mllg <michellang@gmail.com>
2012-05-03 07:47:53 +0000

    fixed generateProblemInstance.R

--- skel/R/generateProblemInstance.R
+++ skel/R/generateProblemInstance.R
@@ -8,16 +8,14 @@
 #' @export
 generateProblemInstance = function(reg, id) {
   job = getJob(reg, id)
-  static = getProblemPart(reg$file.dir, id, "static")
   dynamic.fun = getProblemPart(reg$file.dir, job$prob.id, "dynamic")
   prob.use = c("job", "static") %in% names(formals(dynamic.fun))
-  names(prob.use) = c("job", "static")
-  seed = BatchJobs:::seeder(reg, job$prob.seed)
-  on.exit(seed$reset())
   applyDynamic = switch(as.integer(c(1, 2) %*% prob.use) + 1L,
                         function(...) dynamic.fun(...),
                         function(...) dynamic.fun(job=job, ...),
-                        function(...) dynamic.fun(static=static(), ...),
-                        function(...) dynamic.fun(job=job, static=static(), ...))
+                        function(...) dynamic.fun(static=getProblemPart(reg$file.dir, id, "static"), ...),
+                        function(...) dynamic.fun(job=job, static=getProblemPart(reg$file.dir, id, "static"), ...))
+  seed = BatchJobs:::seeder(reg, job$prob.seed)
+  on.exit(seed$reset())
   do.call(applyDynamic, job$prob.pars)
 }
