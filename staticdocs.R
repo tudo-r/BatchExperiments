cbf10b44646f4891db672ced2694650028fc0d83
berndbischl <bernd_bischl@gmx.net>
2012-05-16 05:46:07 +0000

    move staticdocs to inst

--- staticdocs.R
+++
@@ -1,9 +0,0 @@
-library(staticdocs)
-# see https://github.com/hadley/staticdocs/
-library(BBmisc)
-#options(BBmisc.ProgressBar.style="off")
-
-ddir = "html"
-if(file.exists(ddir))
-  unlink(ddir, recursive = TRUE)
-build_package("BatchExperiments", base_path = ddir)
