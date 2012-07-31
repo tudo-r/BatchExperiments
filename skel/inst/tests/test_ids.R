b9375fb99d33fad37b7dc640d24f195b7f4d02f2
mllg <michellang@gmail.com>
2012-07-31 11:38:35 +0000

    more relaxed id pattern matching ... allow minus (except for registry name)

--- skel/inst/tests/test_ids.R
+++ skel/inst/tests/test_ids.R
@@ -1,12 +1,12 @@
 
 
-test_that("problem / algo ids", {  
+test_that("problem / algo ids", {
   reg = makeTestRegistry()
   expect_error(addProblem(reg, id="."), "comply with")
   expect_error(addProblem(reg, id=".foo"), "comply with")
-  expect_error(addProblem(reg, id="foo-bar"), "comply with")
+  expect_error(addProblem(reg, id="-foor"), "comply with")
 
   expect_error(addAlgorithm(reg, id="."), "comply with")
   expect_error(addAlgorithm(reg, id=".foo"), "comply with")
-  expect_error(addAlgorithm(reg, id="foo-bar"), "comply with")
+  expect_error(addAlgorithm(reg, id="-foobar"), "comply with")
 })
