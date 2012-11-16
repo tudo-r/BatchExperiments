3d18b42f30f2226f5575c925823509bb20e5d7c1
mllg <michellang@gmail.com>
2012-11-16 09:33:29 +0000

    fix for case-insensitive file names, somewhat ugly

--- skel/R/updateExperimentRegistry.R
+++ skel/R/updateExperimentRegistry.R
@@ -1,14 +1,13 @@
 #' @method updateRegistry ExperimentRegistry
 #' @S3method updateRegistry ExperimentRegistry
 updateRegistry.ExperimentRegistry = function(reg) {
-  version.reg = reg$packages$BatchExperiments$version
-  version.pkg = packageVersion("BatchExperiments")
-
   # update the BatchJobs part first
   # FIXME NextMethod() ?
   updated = BatchJobs:::updateRegistry.Registry(reg)
   is.updated = !isFALSE(updated)
 
+  version.reg = reg$packages$BatchExperiments$version
+  version.pkg = packageVersion("BatchExperiments")
   if (!is.updated && version.reg == version.pkg)
     return(FALSE)
   if (version.reg > version.pkg) {
@@ -16,9 +15,31 @@ updateRegistry.ExperimentRegistry = function(reg) {
              version.reg, version.pkg)
     if (!is.updated)
       return(FALSE)
+
+    # return registry which is updated by BatchJobs
+    return(reg)
   }
 
-  # FIXME here is actually nothing to do.
+  if (version.reg < package_version("1.0.767")) {
+    # FIXME fix problem and algorithm file names for windows compability
+    path = file.path(reg$file.dir, "problems")
+    src = list.files(path, full.names = TRUE, pattern="_static\\.RData$")
+    ids = sub("\\.RData$", "", basename(src))
+    dest = sapply(ids, function(id) getProblemFilePaths(reg$file.dir, id)$static)
+    file.rename(src, dest)
+
+    path = file.path(reg$file.dir, "problems")
+    src = list.files(path, full.names = TRUE, pattern="_dynamic\\.RData$")
+    ids = sub("\\.RData$", "", basename(src))
+    dest = sapply(ids, function(id) getProblemFilePaths(reg$file.dir, id)$dynamic)
+    file.rename(src, dest)
+
+    path = file.path(reg$file.dir, "algorithms")
+    src = list.files(path, full.names = TRUE)
+    ids = sub("\\.RData$", "", basename(src))
+    dest = getAlgorithmFilePath(reg$file.dir, ids)
+    file.rename(src, dest)
+  }
 
   reg$packages$BatchExperiments$version = version.pkg
   reg
