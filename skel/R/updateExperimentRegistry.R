b65cbea6667f968ecee3aecbc5d416b9ba2bab6d
mllg <michellang@gmail.com>
2012-11-28 12:52:50 +0000

    cleanups

--- skel/R/updateExperimentRegistry.R
+++ skel/R/updateExperimentRegistry.R
@@ -2,27 +2,23 @@
 #' @S3method updateRegistry ExperimentRegistry
 updateRegistry.ExperimentRegistry = function(reg) {
   # update the BatchJobs part first
-  # FIXME NextMethod() ?
   updated = NextMethod()
-  #updated = BatchJobs:::.Registry(reg)
   is.updated = !isFALSE(updated)
 
   version.reg = reg$packages$BatchExperiments$version
   version.pkg = packageVersion("BatchExperiments")
-  if (!is.updated && version.reg == version.pkg)
-    return(FALSE)
+  if (version.reg == version.pkg) {
+    return(if (is.updated) reg else FALSE)
+  }
+
   if (version.reg > version.pkg) {
     warningf("The registry has been used with BatchExperiments version %s, installed is version %s. You should update BatchExperiments on this machine.",
              version.reg, version.pkg)
-    if (!is.updated)
-      return(FALSE)
-
-    # return registry which is updated by BatchJobs
-    return(reg)
+    return(if (is.updated) reg else FALSE)
   }
 
+  # do updates
   if (version.reg < package_version("1.0.767")) {
-    # FIXME fix problem and algorithm file names for windows compability
     path = file.path(reg$file.dir, "problems")
     src = list.files(path, full.names = TRUE, pattern="_static\\.RData$")
     ids = sub("\\.RData$", "", basename(src))
