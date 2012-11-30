1df11cdd46a8635611736d81d4c9d262aa1aa4f7
berndbischl <bernd_bischl@gmx.net>
2012-11-30 03:51:40 +0000

    improve getJobInfo

--- skel/R/getJobInfo.R
+++ skel/R/getJobInfo.R
@@ -1,58 +1,27 @@
 #' @method getJobInfo ExperimentRegistry
 #' @S3method getJobInfo ExperimentRegistry
-getJobInfo.ExperimentRegistry = function(reg, ids, parameters=FALSE, select, unit = "seconds") {
+getJobInfo.ExperimentRegistry = function(reg, ids, pars=FALSE, prefix.pars=FALSE, select, unit = "seconds") {
   BatchJobs:::syncRegistry(reg)
-  if (!missing(ids))
-    ids = BatchJobs:::checkIds(reg, ids)
-  checkArg(unit, choices=c("seconds", "minutes", "hours", "days", "weeks"))
-  checkArg(parameters, "logical", len=1L, na.ok=FALSE)
-
-  columns = setNames(c("job_id", "prob_id", "algo_id", "repl", "submitted", "started", "done", "done - started AS time_running", "submitted - started AS time_queued", "error", "node", "batch_job_id", "r_pid", "seed"),
-                     c("job.id", "prob", "algo", "repl", "time.submitted", "time.started", "time.done", "time.running", "time.queued", "error.msg", "nodename", "batch.id", "r.pid", "seed"))
-  if (parameters)
-    columns = c(columns, setNames(c("prob_pars", "algo_pars"), c("prob.pars", "algo.pars")))
-
-  if (!missing(select)) {
-    checkArg(select, "character", na.ok=FALSE)
-    columns = columns[names(columns) %in% c("job.id", select)]
-  }
-
-  tab = setNames(BatchJobs:::dbGetExpandedJobsTable(reg, ids, columns), names(columns))
-  if (nrow(tab) == 0L)
-    return(tab)
-
-  # convert times to POSIX
-  if (!is.null(tab$time.submitted))
-    tab$time.submitted = BatchJobs:::dbConvertNumericToPOSIXct(tab$time.submitted)
-  if (!is.null(tab$time.started))
-    tab$time.started = BatchJobs:::dbConvertNumericToPOSIXct(tab$time.started)
-  if (!is.null(tab$time.done))
-    tab$time.done = BatchJobs:::dbConvertNumericToPOSIXct(tab$time.done)
-
-  # shorten error messages
-  if (!is.null(tab$error.msg))
-    tab$error.msg = vapply(tab$error.msg, BatchJobs:::shortenString, "", len = 30L)
-
-  # convert time diffs
-  div = setNames(c(1L, 60L, 3600L, 86400L, 604800L), c("seconds", "minutes", "hours", "days", "weeks"))[unit]
-  if (!is.null(tab$time.running))
-    tab$time.running = prettyNum(tab$time.running / div)
-  if (!is.null(tab$time.queued))
-    tab$time.queued = prettyNum(tab$time.queued / div)
+  checkArg(pars, "logical", len=1L, na.ok=FALSE)
+  columns = c(job.id="job_id", prob="prob_id", algo="algo_id", repl="repl") 
+  if (pars)
+    columns = c(columns, c(prob.pars="prob_pars", algo.pars="algo_pars"))
+  tab = BatchJobs:::getJobInfoInternal(reg, ids, pars, select, unit, columns)
 
   # unserialize parameters
-  if (parameters) {
+  if (pars) {
     if (!is.null(tab$prob.pars)) {
       pars = BatchJobs:::list2df(lapply(tab$prob.pars, function(x) unserialize(charToRaw(x))), force.names=TRUE)
-      names(pars) = sprintf("prob.par.%s", names(pars))
+      if (prefix.pars)
+        names(pars) = sprintf("prob.par.%s", names(pars))
       tab = cbind(subset(tab, select=-prob.pars), pars)
     }
     if (!is.null(tab$algo.pars)) {
       pars = BatchJobs:::list2df(lapply(tab$algo.pars, function(x) unserialize(charToRaw(x))), force.names=TRUE)
-      names(pars) = sprintf("algo.par.%s", names(pars))
+      if (prefix.pars)
+        names(pars) = sprintf("algo.par.%s", names(pars))
       tab = cbind(subset(tab, select=-algo.pars), pars)
     }
   }
-
-  tab
+  return(tab)
 }
--- skel/inst/tests/test_getJobInfo.R
+++ skel/inst/tests/test_getJobInfo.R
@@ -7,32 +7,37 @@ test_that("getJobInfo", {
   a1 = addAlgorithm(r, "A", fun=function(static, dynamic) 1)
   a2 = addAlgorithm(r, "B", fun=function(static, dynamic) 1)
   addExperiments(r, list(makeDesign(p1), makeDesign(p2)), list(makeDesign(a1), makeDesign(a2)), repls=2)
-  submitJobs(r)
-
+  
+  mycheck = function(tab) {
+    expect_true(is.data.frame(tab))
+    expect_equal(tab$id, 1:8)
+    expect_true(nrow(tab) == 8)
+    expect_true(is(tab$time.submitted, "POSIXt"))
+    expect_true(is(tab$time.started, "POSIXt"))
+    expect_true(is(tab$time.done, "POSIXt"))
+    expect_true(is.numeric(tab$time.queued))
+    expect_true(is.numeric(tab$time.running))
+    expect_true(all(is.na(tab$error.msg)))
+    expect_true(is.integer(tab$r.pid))
+    expect_true(is.integer(tab$seed))
+  }
   tab = getJobInfo(r)
-  expect_true(is.data.frame(tab))
-  expect_equal(tab$job.id, 1:8)
-  expect_true(nrow(tab) == 8)
-  expect_true(is(tab$time.submitted, "POSIXt"))
-  expect_true(is(tab$time.started, "POSIXt"))
-  expect_true(is(tab$time.done, "POSIXt"))
-  expect_true(is.character(tab$time.queued))
-  expect_true(is.character(tab$time.running))
-  expect_true(all(is.na(tab$error.msg)))
-  expect_true(is.integer(tab$r.pid))
-  expect_true(is.integer(tab$seed))
+  mycheck(tab)
+  submitJobs(r)
+  tab = getJobInfo(r)
+  mycheck(tab)
 
   tab = getJobInfo(r, ids = integer(0))
   expect_true(is.data.frame(tab))
   expect_true(nrow(tab) == 0L)
 
-  tab = getJobInfo(r, ids = 1, parameters=TRUE)
+  tab = getJobInfo(r, ids = 1, pars=TRUE)
   expect_equal(tab$prob, "one")
   expect_equal(tab$algo, "A")
 
   tab = getJobInfo(r, select = "time.running")
   expect_true(ncol(tab) == 2) # job.id always selected
-  tab = getJobInfo(r, select = c("job.id", "time.running"))
+  tab = getJobInfo(r, select = c("id", "time.running"))
   expect_true(ncol(tab) == 2)
 
   expect_equal(getJobInfo(r)$repl, rep(1:2, 4))
