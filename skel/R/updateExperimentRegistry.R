31841005dcf879ca5e0faff0eb618fc11b3ebf87
mllg <michellang@gmail.com>
2013-01-10 13:13:19 +0000

    updateRegistry fixes

--- skel/R/updateExperimentRegistry.R
+++ skel/R/updateExperimentRegistry.R
@@ -2,7 +2,6 @@
 #' @S3method updateRegistry ExperimentRegistry
 updateRegistry.ExperimentRegistry = function(reg) {
   # update the BatchJobs part first
-  # FIXME NextMethod
   updated = NextMethod()
   is.updated = !isFALSE(updated)
   if (is.updated) reg = updated
@@ -23,14 +22,14 @@ updateRegistry.ExperimentRegistry = function(reg) {
   if (version.reg < package_version("1.0.767")) {
     path = file.path(reg$file.dir, "problems")
     src = list.files(path, full.names = TRUE, pattern="_static\\.RData$")
-    ids = sub("\\.RData$", "", basename(src))
+    ids = sub("_static\\.RData$", "", basename(src))
     dest = vapply(ids, function(id) getProblemFilePaths(reg$file.dir, id)["static"], character(1L))
     file.rename(src, dest)
 
     path = file.path(reg$file.dir, "problems")
     src = list.files(path, full.names = TRUE, pattern="_dynamic\\.RData$")
-    ids = sub("\\.RData$", "", basename(src))
-    dest = vapply(ids, function(id) getProblemFilePaths(reg$file.dir, id)["static"], character(1L))
+    ids = sub("_dynamic\\.RData$", "", basename(src))
+    dest = vapply(ids, function(id) getProblemFilePaths(reg$file.dir, id)["dynamic"], character(1L))
     file.rename(src, dest)
 
     path = file.path(reg$file.dir, "algorithms")
