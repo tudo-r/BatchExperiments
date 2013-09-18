9ac01025e0edeae7480d32e4300693e0c06bc259
berndbischl <bernd_bischl@gmx.net>
2013-09-18 18:10:14 +0000

    BE: add example for findExperiments

--- skel/R/findExperiments.R
+++ skel/R/findExperiments.R
@@ -23,6 +23,16 @@
 #'   Default is \code{TRUE}.
 #' @return [\code{integer}]. Ids for experiments which match the query.
 #' @export
+#' @examples
+#' library(BatchExperiments)
+#' reg <- makeExperimentRegistry(id="example1", file.dir=tempfile())
+#' p1 <- addProblem(reg, "one", 1)
+#' p2 <- addProblem(reg, "two", 2)
+#' a = addAlgorithm(reg, "A", fun=function(static, n) static + n)
+#' addExperiments(reg, algo.design=makeDesign(a, exhaustive = list(n = 1:4)))
+#' findExperiments(reg, prob.pattern="one")
+#' findExperiments(reg, prob.pattern="o")
+#' findExperiments(reg, algo.pars = (n > 2))
 findExperiments = function(reg, ids, prob.pattern, prob.pars, algo.pattern, algo.pars, repls, match.substring=TRUE) {
   checkExperimentRegistry(reg, strict=TRUE)
   if (!missing(prob.pattern))
