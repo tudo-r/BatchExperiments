fea1d557aaf12a205f7b65434513ce337f5a9ad6
Michel <michellang@gmail.com>
2013-11-19 16:24:34 +0100

    gitignore for unittest files

--- inst/tests/helpers.R
+++ inst/tests/helpers.R
@@ -1,11 +1,12 @@
 options(BBmisc.ProgressBar.style="off")
 
 makeTestRegistry = function() {
-  if(unlink("runit_files", recursive=TRUE) != 0)
-    stop("Could not delete runit_files!")
+  dir = "unittests-files"
+  if(unlink(dir, recursive=TRUE) != 0)
+    stop("Could not delete unittests files!")
   makeExperimentRegistry(
     id = "foo",
     seed = 1,
-    file.dir = "runit_files"
+    file.dir = dir
   )
 }
