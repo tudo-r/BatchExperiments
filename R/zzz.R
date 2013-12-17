c084387e07bf9b159ce65cc4a638620da2cd1d6a
Michel <michellang@gmail.com>
2013-12-17 12:49:06 +0100

    new roxygen, added package help page

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,13 @@
+#' The BatchExperiments package
+#'
+#' @section Additional information:
+#'
+#' Wiki, FAQ, examples and more on \url{https://github.com/tudo-r/BatchExperiments}.
+#'
+#' @docType package
+#' @name BatchExperiments
+NULL
+
 #' @import BBmisc
 #' @import DBI
 #' @import RSQLite
