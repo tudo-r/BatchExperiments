c48cc5910fe23b992b64a4b0543c32a20e1751ac
Michel <michellang@gmail.com>
2014-01-28 17:41:16 +0100

    argument external in testJob

--- inst/tests/test_testJob.R
+++ inst/tests/test_testJob.R
@@ -4,5 +4,5 @@ test_that("testJob", {
   p1 = addProblem(reg, "p1", 1)
   a1 = addAlgorithm(reg, id="a1", fun=function(static, dynamic) static)
   addExperiments(reg, p1, a1)
-  expect_equal(testJob(reg, 1L), 1)
+  expect_equal(testJob(reg, 1L, external=FALSE), 1)
 })
