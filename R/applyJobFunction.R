1a0cdac692adbb77c42b97d12222b59e6bf248b6
Renaud <renaud@tx.technion.ac.il>
2015-03-06 13:38:10 +0200

    Fixed indentation

--- R/applyJobFunction.R
+++ R/applyJobFunction.R
@@ -3,7 +3,7 @@
 .applyJobWrapper = function(reg, job, cache, algo) {
   algo.use = c("job", "static", "dynamic")
   algo.use = setNames(algo.use %in% names(formals(algo)), algo.use)
-  
+
   # encapsulate the loading into functions for lazy loading
   #
   # IMPORTANT: Note that the order of the messages in the log files can be confusing.
@@ -13,17 +13,17 @@
   parts = getProblemFilePaths(reg$file.dir, job$prob.id)
   static = function() cache(parts["static"], slot = "static", impute = NULL)
   dynamic = function() calcDynamic(reg, job, static(), cache(parts["dynamic"], slot = "dynamic", impute = NULL))
-  
+
   # switch on algo formals and apply algorithm function
   f = switch(sum(c(1L, 2L, 4L)[algo.use]) + 1L,
-          function(...) algo(...),
-          function(...) algo(job = job, ...),
-          function(...) algo(static = static(), ...),
-          function(...) algo(job = job, static = static(), ...),
-          function(...) algo(dynamic = dynamic(), ...),
-          function(...) algo(job = job, dynamic = dynamic(), ...),
-          function(...) algo(static = static(), dynamic = dynamic(), ...),
-          function(...) algo(job = job, static = static(), dynamic = dynamic(), ...))
+    function(...) algo(...),
+    function(...) algo(job = job, ...),
+    function(...) algo(static = static(), ...),
+    function(...) algo(job = job, static = static(), ...),
+    function(...) algo(dynamic = dynamic(), ...),
+    function(...) algo(job = job, dynamic = dynamic(), ...),
+    function(...) algo(static = static(), dynamic = dynamic(), ...),
+    function(...) algo(job = job, static = static(), dynamic = dynamic(), ...))
   f
 }
 
